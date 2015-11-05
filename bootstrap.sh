a0799003e671c8799cc000b713be295c6711768e
Juliano Fernandes <juliano@smartgreen.net>
2015-11-05 18:42:13 -0200

    ADD Vagrant configuration file, bootstrap script and python requirements file. CHANGE Turning on debug mode and enabling external visibility.

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# install required packages
+apt-get update
+apt-get install -y python3 python3-pip
+
+# API root directory
+API_DIR='/vagrant'
+
+# install required python packages
+pip3 install -r $API_DIR/requirements.txt
--- cebolatol.py
+++ cebolatol.py
@@ -3,8 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return u"Hello Wolld by Cebola ʞ:D"
 
 if __name__ == "__main__":
-    app.run()
-
+    app.run(host='0.0.0.0', debug=True)
