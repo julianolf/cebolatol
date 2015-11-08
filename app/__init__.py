d904e528bb7c7363de68246082420db2bd117946
Juliano Fernandes <julianofernandes@gmail.com>
2015-11-07 23:46:14 -0200

    Adjusts entire user interface

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@
 from flask import Blueprint
 
 # Initializes the blueprint
-app = Blueprint('app', __name__, template_folder='templates')
+app = Blueprint('app', __name__, template_folder='templates', static_folder='static')
 
 # Import application's views (a.k.a. routes)
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from app import app
-from flask import render_template
+from flask import url_for, render_template
 
 
 @app.route('/')
 def index():
 	"""Return Cebolinha Translator WebApp"""
-	return render_template('index.html')
+	github_icon = url_for('.static', filename='github.svg')
+	return render_template('index.html', ic_github=github_icon)
