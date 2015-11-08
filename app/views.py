8d477ad3563942cb34038dbfde0888351b9837fc
Juliano Fernandes <julianofernandes@gmail.com>
2015-11-08 00:14:19 -0200

    Keep the things simple

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from app import app
-from flask import url_for, render_template
+from flask import request, jsonify, url_for, render_template
 
 
 @app.route('/')
@@ -8,3 +8,20 @@ def index():
 	"""Return Cebolinha Translator WebApp"""
 	github_icon = url_for('.static', filename='github.svg')
 	return render_template('index.html', ic_github=github_icon)
+
+@app.route('/api')
+def hello():
+	"""Cebolinha says hello"""
+	return jsonify({'phlase': 'Hello wolld'})
+
+
+@app.route('/api/tlanslate', methods=['POST'])
+def translate():
+	"""Translate messages to Cebolinha's dialect"""
+	if 'message' in request.form:
+		# Just replace all characters 'R' and 'r' for 'L' and 'l' respectively
+		message = request.form['message']
+		phlase = message.replace('R', 'L').replace('r', 'l')
+		return jsonify({'phlase': phlase})
+	else:
+		return jsonify({'ellol': 'You must pass a message'}), 400
--- cebolatol.py
+++ cebolatol.py
@@ -1,15 +1,6 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
-from app import app
-from api import api
-
-# Create new Flask application
-cebolatol = Flask(__name__)
-
-# Register blueprints
-cebolatol.register_blueprint(app)
-cebolatol.register_blueprint(api, url_prefix='/api')
+from app import app, views
 
 if __name__ == "__main__":
 	# Runs server in debug mode with external visibility
-    cebolatol.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
