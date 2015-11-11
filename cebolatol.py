0827a85b645ed5d98acabed77a39b3b95d129701
Luiz Franciso Artigas de Prá <luiz@smartgreen.net>
2015-11-11 18:06:14 -0200

    Changing text and separating js form html

--- cebolatol.py
+++ cebolatol.py
@@ -2,5 +2,5 @@
 from app import app, views
 
 if __name__ == "__main__":
-	# Runs server in debug mode with external visibility
+    # Runs server in debug mode with external visibility
     app.run(host='0.0.0.0', debug=True)
