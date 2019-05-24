f492fceff09491e37d97ecdcdc4119f9b350fd9f
Juliano Fernandes <julianofernandes@gmail.com>
2019-05-24 18:21:38 -0300

    format with black

--- cebolatol.py
+++ cebolatol.py
@@ -3,4 +3,4 @@ from app import app, views
 
 if __name__ == "__main__":
     # Runs server in debug mode with external visibility
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host="0.0.0.0", debug=True)
